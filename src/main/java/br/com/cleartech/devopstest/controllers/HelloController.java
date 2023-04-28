9a7ba18bbb0d1356d4927ce89e6017c366d174ac
Pedro Tomé <ptome@cleartech.com.br>
2023-04-27 21:13:27 -0300

    adicionado log

--- src/main/java/br/com/cleartech/devopstest/controllers/HelloController.java
+++ src/main/java/br/com/cleartech/devopstest/controllers/HelloController.java
@@ -3,13 +3,16 @@ package br.com.cleartech.devopstest.controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.log4j.Log4j2;
 
 @RestController
 @RequestMapping( "/hello" )
+@Log4j2
 public class HelloController {
     
     @GetMapping
     public String index() {
+        log.info( "Hello Devops" );
         return "Hello Devops";
     }
     
