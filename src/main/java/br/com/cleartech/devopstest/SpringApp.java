7662205d53f6b6f3d905c218c22900cb40537e09
Pedro Tomé <ptome@cleartech.com.br>
2023-04-28 18:23:17 -0300

    ajustado nome da classe

--- src/main/java/br/com/cleartech/devopstest/SpringApp.java
+++ src/main/java/br/com/cleartech/devopstest/SpringApp.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StringApp {
+public class SpringApp {
 
 	public static void main(String[] args) {
-		SpringApplication.run(StringApp.class, args);
+		SpringApplication.run(SpringApp.class, args);
 	}
 
 }
