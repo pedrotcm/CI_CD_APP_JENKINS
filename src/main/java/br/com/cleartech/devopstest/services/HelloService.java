f40fe369b468f05a965a26562c0de137e7fb1926
Pedro Tomé <ptome@cleartech.com.br>
2023-05-11 09:03:44 -0300

    adicionado teste random

--- src/main/java/br/com/cleartech/devopstest/services/HelloService.java
+++ src/main/java/br/com/cleartech/devopstest/services/HelloService.java
@@ -1,5 +1,6 @@
 package br.com.cleartech.devopstest.services;
 
+import java.util.Random;
 import org.springframework.stereotype.Service;
 import lombok.extern.log4j.Log4j2;
 
@@ -8,7 +9,8 @@ import lombok.extern.log4j.Log4j2;
 public class HelloService {
 
     public String hello() {
-        log.info( "Hello Devops" );
+        var random = new Random();
+        log.info( "Hello Devops {}", random.nextInt() );
         return "Hello Devops";
     }
 
